8efef1f815a495d3d597720dc9aaa7ae4d0a48f4
Dione Guielebo <dione@direct.one>
2022-01-21 11:04:52 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentdate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Helo, {name}, on {currentdate:d} at {currentdate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
