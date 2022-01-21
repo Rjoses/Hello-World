0113d32c26cf75cb3f203772b7777e043b3604d7
Roney José <roney.fut@gmail.com>
2022-01-21 12:00:35 -0300

    correção do texto hello

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace hello_world
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
             var currentdate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Helo, {name}, on {currentdate:d} at {currentdate:t}!");
-            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentdate:d} at {currentdate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit.........");
             Console.ReadKey(true);
         }
     }
